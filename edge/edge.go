56138023e9cdb458b4c6a53cbc1dab06cbe46489
Ruben M. Damião <rmdamiao@gmail.com>
2023-02-17 19:37:02 -0300

    updating interface

--- edge/edge.go
+++ edge/edge.go
@@ -2,6 +2,7 @@ package edge
 
 import (
 	"errors"
+	"io"
 
 	"github.com/lienkolabs/aereum/core/crypto"
 	"github.com/lienkolabs/aereum/core/crypto/dh"
@@ -16,13 +17,15 @@ const (
 	MODERATED = 1 << 2
 )
 
-var InsufficientKnowledgeError = errors.New("user does not have knowledge to perform action")
+var (
+	InsufficientKnowledgeError = errors.New("user does not have knowledge to perform action")
+	StageNotFoundError         = errors.New("stage not found")
+)
 
 type Author struct {
 	Author    crypto.Token
 	Attorney  crypto.Token
 	Signer    crypto.PrivateKey
-	Wallet    crypto.PrivateKey
 	Stages    map[crypto.Token]*Stage
 	Secrets   *SecureVault
 	Ephemeral map[crypto.Token]crypto.Token
@@ -32,8 +35,63 @@ type Author struct {
 
 // Unencrypted Content
 type Content struct {
+	Author      crypto.Token
+	Stage       crypto.Token
+	Sponsored   bool
+	Encrypted   bool
 	ContentType string
 	Content     []byte
+}
+
+type NullWriterCloser struct{}
+
+func (n NullWriterCloser) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
+func (n NullWriterCloser) Close() error {
+	return nil
+}
+
+func NewContentMemoryStore(cache int) *ContentStore {
+	return &ContentStore{
+		io:        NullWriterCloser{},
+		content:   make([]Content, 0),
+		cacheSize: cache,
+	}
+}
+
+func OpenContentDiskStore(cache int, filePath string) (*ContentStore, error) {
+	return nil, nil
+}
+
+func NewContentDiskStore(cache int, filePath string) (*ContentStore, error) {
+	return nil, nil
+}
+
+type ContentStore struct {
+	io        io.WriteCloser
+	content   []Content
+	cacheSize int
+}
+
+func (c *ContentStore) AppendContent(content *instructions.Content) error {
+	newContent := Content{
+		Encrypted:   content.Encrypted,
+		Sponsored:   content.Sponsored,
+		ContentType: content.ContentType,
+		Content:     content.Content,
+	}
+	if len(c.content) < c.cacheSize {
+		c.content = append(c.content, newContent)
+	} else {
+		c.content = append(c.content[:len(c.content)-1], newContent)
+	}
+	bytes := content.Serialize()
+	if n, err := c.io.Write(bytes); n != len(bytes) {
+		return err
+	}
+	return nil
 }
 
 // Stage keep information about stages.
@@ -45,13 +103,17 @@ type Stage struct {
 	Flag        byte
 	Description string
 	Public      bool
-	Submission  crypto.Token
-	Moderation  crypto.Token
-	Readers     map[crypto.Token]crypto.Token
-	Submitors   map[crypto.Token]crypto.Token
-	Moderators  map[crypto.Token]crypto.Token
-	Content     []Content
+	Submission  TokenHistory
+	Moderation  TokenHistory
+	Content     *ContentStore
+	Members     *StageMembers
 	Live        bool
+}
+
+type StageMembers struct {
+	Readers    map[crypto.Token]crypto.Token
+	Submitors  map[crypto.Token]crypto.Token
+	Moderators map[crypto.Token]crypto.Token
 }
 
 type DateAndSigner interface {
@@ -59,15 +121,61 @@ type DateAndSigner interface {
 	Sign(key crypto.PrivateKey)
 }
 
+// TokenHistory is a reverse history of tokens. The first token is from an epoch
+// after the second token and so on.
+type TokenHistory []EpochToken
+
+func (t TokenHistory) Append(epoch uint64, token crypto.Token) {
+	for n, epochToken := range t {
+		if epochToken.Epoch < epoch {
+			return epochToken.Token, true
+		}
+	}
+
+}
+
+func (t TokenHistory) Token(epoch uint64) (crypto.Token, bool) {
+	for _, epochToken := range t {
+		if epochToken.Epoch < epoch {
+			return epochToken.Token, true
+		}
+	}
+	return crypto.ZeroToken, false
+}
+
+type EpochToken struct {
+	Epoch uint64
+	Token crypto.Token
+}
+
+type Drama struct {
+	Stages    map[crypto.Token]*Stage
+	ContentIO io.WriteCloser
+	StagesIO  io.WriteCloser
+}
+
+func (d Drama) AppendContent(content instructions.Content) {
+
+}
+
+func (d Drama) IncorporateContent()
+
 func (a *Author) CreateStage(description string, permissions, flag byte) (*instructions.CreateStage, error) {
 	owner, err := a.Secrets.NewKey()
 	if err != nil {
 		return nil, err
 	}
-	secrets := StageSecrets{
-		Ownership: owner,
+	stage := Stage{
+		Token:       owner.PublicKey(),
+		Flag:        flag,
+		Description: description,
+		Readers:     make(map[crypto.Token]crypto.Token),
+		Submitors:   make(map[crypto.Token]crypto.Token),
+		Moderators:  make(map[crypto.Token]crypto.Token),
+		Content:     make([]Content, 0),
+		Live:        false,
 	}
-	stage := instructions.CreateStage{
+	create := instructions.CreateStage{
 		Author:      a.Author,
 		Stage:       owner.PublicKey(),
 		Description: description,
@@ -75,15 +183,37 @@ func (a *Author) CreateStage(description string, permissions, flag byte) (*instr
 		Attorney:    a.Attorney,
 	}
 	if (permissions & ENCRYPTED) == 1 {
-		secrets.Cipher = crypto.NewCipherKey()
+		if stage.CipherKey, err = a.Secrets.NewCipherKey(); err != nil {
+			return nil, err
+		}
 	}
 	if (permissions & CLOSED) == 1 {
-		stage.Submission, secrets.Submission = crypto.RandomAsymetricKey()
+		if prvKey, err := a.Secrets.NewKey(); err != nil {
+
+			return nil, err
+		} else {
+			stage.Submission = prvKey.PublicKey()
+			create.Submission = stage.Submission
+		}
 	}
 	if (permissions & MODERATED) == 1 {
-		stage.Moderation, secrets.Submission = crypto.RandomAsymetricKey()
+		if prvKey, err := a.Secrets.NewKey(); err != nil {
+			return nil, err
+		} else {
+			stage.Moderation = prvKey.PublicKey()
+			create.Moderation = stage.Moderation
+		}
 	}
-	return &stage, nil
+	return &create, nil
+}
+
+func (a *Author) StageCreated(create *instructions.CreateStage) error {
+	stage, ok := a.Stages[create.Stage]
+	if !ok {
+		return StageNotFoundError
+	}
+	stage.Live = true
+	return nil
 }
 
 func (a *Author) JoinStage(stage crypto.Token, introduction string) (*instructions.JoinStage, crypto.PrivateKey) {
